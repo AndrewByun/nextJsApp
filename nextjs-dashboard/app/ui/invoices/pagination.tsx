1817c64c3922b491193cf5eb3916da135a7e2f3c
AndrewByun <andrewbyun1@gmail.com>
2024-02-16 11:13:52 -0800

    testing git config

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -18,6 +18,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathName}?${params.toString()}`;
   }
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
